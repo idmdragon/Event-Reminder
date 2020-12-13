f7bb5c5bb3c24d56ed7ce99d51b0feb044bb179e
Mohammad Zulfikar <48849323+mohzulfikar@users.noreply.github.com>
2020-12-13 14:11:06 +0700

    Add companion object

--- app/src/main/java/com/example/eventreminder/InputActivity.kt
+++ app/src/main/java/com/example/eventreminder/InputActivity.kt
@@ -25,7 +25,12 @@ class InputActivity : AppCompatActivity(), View.OnClickListener {
         const val EXTRA_NOTE = "extra_note"
         const val EXTRA_POSITION = "extra_position"
         const val REQUEST_ADD = 100
+        const val RESULT_ADD = 101
         const val REQUEST_UPDATE = 200
+        const val RESULT_UPDATE = 201
+        const val RESULT_DELETE = 301
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
     }
     lateinit var btnEventDate : LinearLayout
     lateinit var btnEventTime : LinearLayout
