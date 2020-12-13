d388e14d31654b004d085e4d83fc8495b9d2df9d
idmdragon <ilham.uklison@gmail.com>
2020-12-13 21:36:17 +0700

    alarm

--- app/src/main/java/com/example/eventreminder/InputActivity.kt
+++ app/src/main/java/com/example/eventreminder/InputActivity.kt
@@ -1,21 +1,16 @@
 package com.example.eventreminder
 
-import android.app.TimePickerDialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.LinearLayout
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
 import kotlinx.android.synthetic.main.activity_input.*
-import kotlin.math.log
 
 class InputActivity : AppCompatActivity(), View.OnClickListener {
     private var isEdit = false
@@ -30,22 +25,28 @@ class InputActivity : AppCompatActivity(), View.OnClickListener {
         const val RESULT_UPDATE = 201
         const val RESULT_DELETE = 301
         const val ALERT_DIALOG_CLOSE = 10
+
+        private const val DATE_PICKER_TAG = "DatePicker"
+        private const val TIME_PICKER_ONCE_TAG = "TimePickerOnce"
+        private const val TIME_PICKER_REPEAT_TAG = "TimePickerRepeat"
         const val ALERT_DIALOG_DELETE = 20
     }
-    lateinit var btnEventDate : LinearLayout
-    lateinit var btnEventTime : LinearLayout
-    lateinit var tvPickEventDate : TextView
-    lateinit var tvPickEventTime : TextView
+
+
+    private lateinit var alarmReceiver: AlarmReceiver
+
     lateinit var etInputTanggal : TextInputEditText
+
+    var dateMonthList = arrayOf("Jan", "Feb", "Mar",
+            "Apr", "May", "Jun", "Jul", "Aug",
+            "Sep", "Oct", "Nov", "Dec")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input)
 
-        btnEventDate = findViewById(R.id.btnEventDate)
-        tvPickEventDate = findViewById(R.id.tvPickEventDate)
-        tvPickEventTime = findViewById(R.id.tvPickEventTime)
-        btnEventTime = findViewById(R.id.btnEventTime)
-        etInputTanggal = findViewById(R.id.etInputTanggal)
+
+        etInputTanggal = findViewById(R.id.etInputTanggalEvent)
         supportActionBar?.title = "Create Reminder"
 
         supportActionBar?.setDisplayShowHomeEnabled(true)
@@ -66,15 +67,15 @@ class InputActivity : AppCompatActivity(), View.OnClickListener {
         val btnTitle: String
 
         if (isEdit) {
-            actionBarTitle = "Edit"
+            actionBarTitle = "Edit Reminder"
             btnTitle = "Update"
 
             event?.let {
                 etInputEventName.setText(it.eventName)
-                tvPickEventDate.text = it.eventDate
-                tvPickEventTime.text = it.eventTime
-                tvReminderDate.text = it.reminderDate
-                tvReminderTime.text = it.reminderTime
+                etInputTanggalEvent.setText(it.eventDate)
+                etInputWaktuEvent.setText(it.eventTime)
+                etInputTanggalReminder.setText(it.reminderDate)
+                etInputWaktuReminder.setText(it.reminderTime)
                 etAdditionalNote.setText(it.keterangan)
             }
         } else {
@@ -89,29 +90,102 @@ class InputActivity : AppCompatActivity(), View.OnClickListener {
         btn_save.text = btnTitle
         btn_save.setOnClickListener(this)
 
-        val builder = MaterialDatePicker.Builder.datePicker()
-        val picker = builder.build()
-        val dateBuilder = MaterialTimePicker.Builder()
-        val materialTimePicker = dateBuilder.setTimeFormat(TimeFormat.CLOCK_24H)
+
+        //Event Date
+        val builderEventDate = MaterialDatePicker.Builder.datePicker()
+        val pickerEventDate = builderEventDate.build()
+
+        //Event Time
+        val pickerEventTimeBuilder = MaterialTimePicker.Builder()
+        val EventTimePicker = pickerEventTimeBuilder.setTimeFormat(TimeFormat.CLOCK_24H)
                 .build()
 
-        btnEventDate.setOnClickListener {
-            picker.show(supportFragmentManager, picker.toString())
-            picker.addOnPositiveButtonClickListener {
-                tvPickEventDate.text = picker.headerText
+        //Reminder Date
+        val builderReminderDate = MaterialDatePicker.Builder.datePicker()
+        val pickerReminderDate = builderReminderDate.build()
+
+        //Reminder Time
+        val pickerReminderTimeBuilder = MaterialTimePicker.Builder()
+        val EventReminderPicker = pickerReminderTimeBuilder.setTimeFormat(TimeFormat.CLOCK_24H)
+                .build()
+
+
+        etInputTanggalEvent.setOnClickListener {
+            pickerEventDate.show(supportFragmentManager, pickerEventDate.toString())
+            pickerEventDate.addOnPositiveButtonClickListener {
+                val inputTanggal = pickerEventDate.headerText.substring(0, 5)
+                etInputTanggalEvent.setText(inputTanggal)
+
+            }
+
+        }
+
+        etInputWaktuEvent.setOnClickListener {
+            EventTimePicker.show(supportFragmentManager, "fragment_tag")
+            EventTimePicker.addOnPositiveButtonClickListener {
+                if (EventTimePicker.hour in 1..9){
+                    val jam = "0$EventTimePicker"
+                    val time = "${EventTimePicker.hour}:${EventTimePicker.minute}"
+                    etInputWaktuEvent.setText(time)
+                }else{
+                    val time = "${EventTimePicker.hour}:${EventTimePicker.minute}"
+                    etInputWaktuEvent.setText(time)
+                }
             }
         }
 
-        btnEventTime.setOnClickListener {
-            materialTimePicker.show(supportFragmentManager, "fragment_tag")
-            materialTimePicker.addOnPositiveButtonClickListener {
-                val time = "${materialTimePicker.hour}.${materialTimePicker.minute}"
-                tvPickEventTime.text = time
-                etInputTanggal.setText(time)
-                Log.d("Debug", time)
-            }
+        var dateRawFormat = ""
+        var dateFormatted : MutableList<String> = arrayListOf()
 
+        etInputTanggalReminder.setOnClickListener {
+            pickerReminderDate.show(supportFragmentManager, pickerReminderDate.toString())
+            pickerReminderDate.addOnPositiveButtonClickListener {
+                etInputTanggalReminder.setText(pickerReminderDate.headerText)
+                dateRawFormat = pickerReminderDate.headerText
+                dateFormatted = dateRawFormat.split(" ") as MutableList<String>
+                dateFormatted.set(0, dateMonthList.indexOf(dateFormatted[0]).toString())
+                dateFormatted.set(1, dateFormatted[1].replace(",", ""))
+                dateFormatted.set(2, dateFormatted[2])
+
+            }
+//            etInputTanggalReminder.setText("2020-12-13")
         }
+
+
+
+
+        etInputWaktuReminder.setOnClickListener {
+            EventReminderPicker.show(supportFragmentManager, "fragment_tag")
+            EventReminderPicker.addOnPositiveButtonClickListener {
+                val time = "${EventReminderPicker.hour}.${EventReminderPicker.minute}"
+                etInputWaktuReminder.setText(time)
+            }
+        }
+
+        alarmReceiver = AlarmReceiver()
+
+
+
+
+        btn_save.setOnClickListener {
+//            Toast.makeText(applicationContext, etInputTanggalReminder.text.toString(),Toast.LENGTH_LONG).show()
+            val date = dateFormatted.toString()
+            val time = "21:04"
+            val message = "2020-12-13"
+
+
+
+            alarmReceiver.setOneTimeAlarm(this, AlarmReceiver.TYPE_ONE_TIME,
+                    date,
+                    time,
+                    message)
+//                etInputTanggalReminder.text.toString(),
+//                etInputWaktuReminder.text.toString(),
+        }
+
+
+
+
     }
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
@@ -119,9 +193,9 @@ class InputActivity : AppCompatActivity(), View.OnClickListener {
     }
     override fun onClick(p0: View?) {
         if (position == 0) {
-            Toast.makeText(this, "Halo" , Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Halo", Toast.LENGTH_LONG).show()
         } else {
-            Toast.makeText(this, "U Clicked Me!" , Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "U Clicked Me!", Toast.LENGTH_LONG).show()
         }
     }
 }
