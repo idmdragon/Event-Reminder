d8198e6e6f1decbdb4abef03c6de50830c097005
Mohammad Zulfikar <48849323+mohzulfikar@users.noreply.github.com>
2020-12-13 22:10:45 +0700

    Add Remove Function

--- app/src/main/java/com/example/eventreminder/EventAdapter.kt
+++ app/src/main/java/com/example/eventreminder/EventAdapter.kt
@@ -6,11 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_name.view.*
 
-class EventAdapter(var listEvent : List<Event>, val activity: Activity):RecyclerView.Adapter<EventAdapter.EventViewHolder>(){
+class EventAdapter(var listEvent : MutableList<Event>, val activity: Activity):RecyclerView.Adapter<EventAdapter.EventViewHolder>(){
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
@@ -65,7 +67,11 @@ class EventAdapter(var listEvent : List<Event>, val activity: Activity):Recycler
                     }
                 }))
 
-
+                btn_delete.setOnClickListener(CustomOnItemClickListener(adapterPosition, object : CustomOnItemClickListener.OnItemClickCallback {
+                    override fun onItemClicked(view: View, position: Int) {
+                        showAlertDialog(position)
+                    }
+                }))
 
                 toggleSubpractices.setOnClickListener(CustomOnItemClickListener(adapterPosition, object : CustomOnItemClickListener.OnItemClickCallback{
 
@@ -82,15 +88,35 @@ class EventAdapter(var listEvent : List<Event>, val activity: Activity):Recycler
 
                     }
                 }))
-
-
             }
-
-              fun changeIcon(imageView: ImageView, int: Int) {
+            fun changeIcon(imageView: ImageView, int: Int) {
                 imageView.setImageResource(int)
             }
         }
     }
+    private fun removeItem(position: Int){
+        listEvent.removeAt(position)
+        notifyDataSetChanged()
+    }
+    private fun showAlertDialog(pos: Int) {
+        val dialogTitle: String
+        val dialogMessage: String
 
+        dialogMessage = "Apakah anda yakin ingin menghapus event ini?"
+        dialogTitle = "Hapus Event"
+
+        val alertDialogBuilder = AlertDialog.Builder(activity)
+        alertDialogBuilder.setTitle(dialogTitle)
+        alertDialogBuilder
+                .setMessage(dialogMessage)
+                .setCancelable(false)
+                .setPositiveButton("Ya") { dialog, id ->
+                    val result = removeItem(pos)
+                    Toast.makeText(activity, "Berhasil menghapus data", Toast.LENGTH_SHORT).show()
+                }
+                .setNegativeButton("Tidak") { dialog, id -> dialog.cancel() }
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
 }
 
